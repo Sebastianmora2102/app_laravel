3d8b1304456602071cfa81033d7009310f218d23
Sebastianmora2102 <juniorfrias2120@gmail.com>
2023-05-28 14:02:59 -0500

    creación de controladores y vistas

---
+++ resources/views/vista1.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Home</title>
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+</head>
+<body>
+    <h1>Welcome to the Home Page</h1>
+    <!-- Agrega aquí el contenido adicional de la vista -->
+    <script src="{{ asset('js/app.js') }}"></script>
+</body>
+</html>
---
+++ resources/views/vista3.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Titulo</title>
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+</head>
+<body>
+    <h1>Vista</h1>
+    <!-- Agrega aquí el contenido adicional de la vista -->
+    <script src="{{ asset('js/app.js') }}"></script>
+</body>
+</html>
