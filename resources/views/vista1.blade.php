9b03b178dfccfe81d139216808547ee67123abaf
Sebastianmora2102 <juniorfrias2120@gmail.com>
2023-05-28 16:08:06 -0500

    diseño primera vista

--- resources/views/vista1.blade.php
+++ resources/views/vista1.blade.php
@@ -1,12 +1,70 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
-    <title>Home</title>
-    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Aplicación Web</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Russo One&display=swap">
+
 </head>
 <body>
-    <h1>Welcome to the Home Page</h1>
-    <!-- Agrega aquí el contenido adicional de la vista -->
-    <script src="{{ asset('js/app.js') }}"></script>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark" id="navbar">
+        <div class="container">
+            <a class="navbar-brand" href="/">Galeria de viajes</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link" href="/about">Acerca de</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="/contact">Contacto</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="/product">Producto</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="/gallery">Galería</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="/services">Servicios</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+    <style>
+    .background-image {
+        background-image: url('/imagenes/fondo1.jpg');
+        background-repeat: no-repeat;
+        background-size: cover;
+        background-position: center;
+        min-height: calc(100vh - 56px);
+    }
+    .display-4 {
+        font-family: 'Russo One', sans-serif; 
+        color: #ffffff; 
+    }
+    </style>
+
+    <div class="background-image">
+        <div class="d-flex justify-content-center align-items-center" style="height: 90vh;">
+        <div class="mr-3">
+            <img src="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.6.0/icons/arrow-up.svg" alt="Flecha hacia arriba" width="150" height="150">
+        </div>
+        <h1 class="display-4 text-center">SELECCIONE UNA DE LAS CATEGORÍAS</h1>
+        <div class="ml-3">
+            <img src="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.6.0/icons/arrow-up.svg" alt="Flecha hacia arriba" width="150" height="150">
+        </div>
+        </div>
+    </div>
+  
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.7/dist/umd/popper.min.js" integrity="sha384-zYPOMqeu1DAVkHiLqWBUTcbYfZ8osu1Nd6Z89ify25QV9guujx43ITvfi12/QExE" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.min.js" integrity="sha384-Y4oOpwW3duJdCWv5ly8SCFYWqFDsfob/3GkgExXKV4idmbt98QcxXYs9UoXAB7BZ" crossorigin="anonymous"></script>
 </body>
 </html>
